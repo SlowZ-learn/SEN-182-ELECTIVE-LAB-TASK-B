dce813cace2f2b5ccb4f9adca4092f295f08d55e
Ohasi-Ekeocha Nnamdi <nnamdinumber1@gmail.com>
2026-05-16 13:36:10 +0100

    I arranged the folders properly

---
+++ CSS positioning/modal.js
@@ -0,0 +1,14 @@
+const modal = document.querySelector(".modal-form")
+const openModalBtn = document.querySelector(".open-modal-btn")
+const closeModalBtn = document.querySelector(".cancel-btn")
+
+
+
+openModalBtn.addEventListener('click',() =>{
+    modal.classList,remove('hidden')
+})
+
+openModalBtn.addEventListener('click',() =>{
+    modal.classList,add('hidden')
+})
+
